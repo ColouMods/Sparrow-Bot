7b146b1f9a3399ed3ae06ee66caa2b4d35dcff05
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-17 17:50:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ client.on('message', message => {
 		if (message.content.match(/cat/i)) 
     {
 		catNum = (Math.floor(Math.random() * 4)+1);//this is the number of possibilities starting from zero, so 5 is 01234
-        message.channel.sendMessage("https://raw.githubusercontent.com/ColouMods/Loren-Bot/master/cats/cat"+catNum+".png");
+        message.channel.sendMessage("https://raw.githubusercontent.com/ColouMods/Sparrow-Bot/master/cats/cat"+catNum+".png");
 		return;
     }
     if (message.content.match(/steamed hams/i) ||
