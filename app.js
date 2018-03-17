3167238b02a392b88e8087edfddd9e3bbe29d844
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-17 19:21:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,13 +14,11 @@ client.on('message', message => {
 		return;
 	}	
 	if (message.content.match(/food/i) ||
-        message.content.match(/steak/i) ||
-		message.content.match(/cheese/i) ||
-		message.content.match(/dinner/i) ||
-		message.content.match(/lunch/i) ||
-		message.content.match(/breakfast/i) ||
-		message.content.match(/meatballs/i) ||
-		message.content.match(/tacos/i)
+	    message.content.match(/kitchen/i) ||
+	    message.content.match(/dinner/i) ||
+	    message.content.match(/lunch/i) ||
+	    message.content.match(/breakfast/i) ||
+	    message.content.match(/ramsay/i)
         )
     	{
         message.channel.sendMessage("Kitchen Nightmares lmfao  https://raw.githubusercontent.com/ColouMods/Sparrow-Bot/master/images/ramsay.jpg");
