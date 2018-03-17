9b7895e91891cf1f174c88c11bb5465d97c3a850
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-17 19:29:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ client.on('message', message => {
 	if (message.content.match(/@sparrow/i))
 		if (message.content.match(/@sparrow view github/i))
 	{
-        message.channel.sendMessage("My GitHub link is https://github.com/ColouMods/Sparrow-Bot/ Wait, I don't have a Github link, I'm Evie Whateverthefuckmylastnameis.");
+        message.channel.sendMessage("My GitHub link is https://github.com/ColouMods/Sparrow-Bot/ Wait, I don't have a Github link, I'm Sparrow.");
         }
 		else if (message.content.match(/@sparrow quote the lion king/i))
 	{
