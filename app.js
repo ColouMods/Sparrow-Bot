8642dac88ded516370cda6a1bc5232f067c3ad79
ColouMods <32771001+ColouMods@users.noreply.github.com>
2018-03-29 00:06:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ return;
 });
 //This is for responding to Chris
 client.on('message', message => {
-	if (message.author.username == "Chris") {
+	if (message.author.id == 333710766706524167) {
 		if (message.content.match(/<@424679471077916682>/i)) {
 			if (message.content.match(/die/i)) {
 				if (diebot == 20)
