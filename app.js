899c5248448d800713f93a53e5336fed3d78f138
DocBison <37480191+DocBison@users.noreply.github.com>
2018-06-24 14:21:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -518,7 +518,7 @@ client.on('message', message => {
 		msgChannel = sendChannels[wrongChannel];
 		client.channels.get(msgChannel).sendMessage("https://raw.githubusercontent.com/ColouMods/Sparrow-Bot/master/images/cat"+catNum+".jpg");
        		//if (sendChannels[wrongChannel].guild_id != rightChannel.guild_id) {
-		client.channels.get(msgChannel).send(msgChannel.guild_id +", " + rightChannel);	
+		client.channels.get(msgChannel).send(msgChannel.guild.id +", " + rightChannel);	
 		//client.channels.get(sendChannels[wrongChannel]).send("Whoops, wrong server.");
 		//}
 		return;
