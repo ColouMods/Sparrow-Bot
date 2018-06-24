20f127bd8976f5b66b2186a869e2c80be1da537e
DocBison <37480191+DocBison@users.noreply.github.com>
2018-06-24 13:40:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -513,8 +513,8 @@ client.on('message', message => {
 	else if (wrongNum == 1){
 		wrongChannel = (Math.floor(Math.random() * sendChannels.length));
 		catNum = (Math.floor(Math.random() * 56)+1);//this is the number of possibilities starting from zero, so 5 is 01234
-		client.channels.get('wrongChannel').sendMessage("https://raw.githubusercontent.com/ColouMods/Sparrow-Bot/master/images/cat"+catNum+".jpg");
-       		client.channels.get('wrongChannel').send("Whoops, wrong server.");
+		client.channels.get(wrongChannel).sendMessage("https://raw.githubusercontent.com/ColouMods/Sparrow-Bot/master/images/cat"+catNum+".jpg");
+       		client.channels.get(wrongChannel).send("Whoops, wrong server.");
 		return;
 	}
 	}
