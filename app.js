0b89c723a41b19dfee6b277169d040c53d00763c
DocBison <37480191+DocBison@users.noreply.github.com>
2018-07-19 16:44:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ bitch = '"Damn Kelly was such a bitch lmao"';
 //this is just to check if she'll come back online
 client.on('ready', () => {
 	console.log('I am ready!');
+	client.user.setActivity({game: {name: "Sims 2 Ultimate Collection", type: 0}});
 	client.channels.get('449616538358513698').sendMessage("I've updated!");
     //client.user.setActivity('Sims 2 Ultimate Collection', { type: 'PLAYING' });
     /*client.user.setActivity('YouTube', { type: 'WATCHING' })
